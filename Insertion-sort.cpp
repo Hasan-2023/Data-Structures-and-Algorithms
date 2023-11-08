b6fd806f17d6b1071434c2a90c5e59ed5c9c02dd
Md. Hasan <123643568+Hasan-2023@users.noreply.github.com>
2023-11-08 19:32:54 +0600

    Create Insertion-sort.cpp
    This is insertion sort algorithm.

---
+++ Insertion-sort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+void insertionshort(int arr[])
+{
+    int j = 0;
+    int key;
+
+    for(int i =1; i<5; i++)
+    {
+        key = arr[i];
+        j = i-1;
+
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1] = arr[j];
+            j = j-1;
+
+        }
+        arr[j+1] = key;
+
+    }
+
+}
+int main()
+{
+    int myarray[5];
+    cout<<"Enter 5 integer in any order"<<endl;
+
+    for(int i = 0; i<5; i++)
+    {
+        cin>>myarray[i];
+    }
+    cout<<"Before sorting"<<endl;
+    for(int i=0; i<5; i++)
+    {
+        cout<<myarray[i]<<" ";
+
+    }
+
+    insertionshort(myarray);
+    cout<<endl;
+
+    cout<<"After sorting"<<endl;
+    for(int i=0; i<5; i++)
+    {
+        cout<<myarray[i]<<" ";
+
+    }
+
+    return 0;
+}
