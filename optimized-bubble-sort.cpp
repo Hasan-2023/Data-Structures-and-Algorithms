67e619b9c581cd683a7d20402a69d49c4b917632
Md. Hasan <123643568+Hasan-2023@users.noreply.github.com>
2023-11-08 19:36:15 +0600

    Create optimized-bubble-sort.cpp
    This is optimized bubble sort algorithm.

---
+++ optimized-bubble-sort.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+
+    void bubblesort(int a[])
+    {
+        int rounds = 0;
+        for(int i=0; i<5; i++)
+        {
+            rounds++;
+            for(int j=0; j<(5-i-1); j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        cout<<"Number of rounds "<<rounds<<endl;
+
+    }
+
+    void optimizedbubblesort(int a[])
+    {
+        int rounds = 0;
+
+        for(int i=0; i<5; i++)
+        {
+            rounds++;
+            int flag = false;
+            for(int j=0; j<(5-i-1); j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    flag = true;
+
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+            if(flag == false)
+            {
+                break;
+            }
+        }
+        cout<<"Number of rounds "<<rounds<<endl;
+    }
+
+int main()
+{
+    int myarray[5];
+
+    cout<<"Enter 5 integer in any order"<<endl;
+
+    for(int i=0; i<5; i++)
+    {
+        cin>>myarray[i];
+    }
+
+    cout<<"Before sorting"<<endl;
+    for(int i=0; i<5; i++)
+
+    {
+        cout<<myarray[i]<<" ";
+
+    }
+    cout<<endl;
+    optimizedbubblesort(myarray);
+
+    cout<<"After sorting"<<endl;
+    for(int i=0; i<5; i++)
+
+    {
+        cout<<myarray[i]<<" ";
+    }
+
+
+
+    return 0;
+}
